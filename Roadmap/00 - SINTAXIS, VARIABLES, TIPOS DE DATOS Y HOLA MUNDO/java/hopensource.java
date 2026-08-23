6c413966f18f34a3ec95b99ac855bf6cb26908eb
Paloma Mesón de Arana <hopensource@tutamail.com>
2026-08-23 12:55:46 +0200

    start firts exercise

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/hopensource.java
@@ -0,0 +1,18 @@
+// https://www.java.com/es/
+
+// En java, para comentar en una sola línea se utilizan las dos barras //
+
+/*
+Pero si queremos escribir comentarios multilínea tenemos esta otra opción
+sin necesidad de estar escribiendo la doble barrita repetidamente.
+*/
+
+public class hopensource {
+    
+    // crear una variable 
+    int variable = 1;
+
+    // crear una constante
+    final int constante = 2;   // No termino de entender la diferencia entre "final" vs. "static".
+
+}
